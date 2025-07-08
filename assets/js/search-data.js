066f69afaa2774a6992d15e6b839ff6ca12e7943
mitchellostrow <35669245+mitchellostrow@users.noreply.github.com>
2025-07-08 15:31:27 +0000

    Deploying to gh-pages from @ mitchellostrow/mitchellostrow.github.io@9a37f33efc3c79b2436386e97c91425f62ed4f2d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,8 +37,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/writing/";
           },
-        },{id: "nav-music",
-          title: "music",
+        },{id: "nav-albums-i-like",
+          title: "albums I like",
           description: "",
           section: "Navigation",
           handler: () => {
